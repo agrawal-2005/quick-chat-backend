39d42c7c73916294406cac803d782d9b3415de37
Prashant Agrawal <agrawalprashant906@gmail.com>
2025-01-22 16:59:43 +0530

    make final changes

--- src/index.js
+++ src/index.js
@@ -24,15 +24,15 @@ module.exports = {
     });
 
     io.on("connection", (socket) => {
-      console.log("A user connected:", socket.id);
+      // console.log("A user connected:", socket.id);
 
       socket.on("user-msg", (msgs) => {
-        console.log("Message received:", msgs);
+        // console.log("Message received:", msgs);
         socket.emit("message", msgs);
       });
 
       socket.on("disconnect", () => {
-        console.log("A user disconnected:", socket.id);
+        // console.log("A user disconnected:", socket.id);
       });
     });
 
