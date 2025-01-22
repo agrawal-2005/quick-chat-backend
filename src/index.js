a1d7eaf154d7290241f17d798d5fdc20b1f01a58
Prashant Agrawal <agrawalprashant906@gmail.com>
2025-01-22 16:53:19 +0530

    update index.js file

--- src/index.js
+++ src/index.js
@@ -4,43 +4,38 @@ module.exports = {
   /**
    * An asynchronous register function that runs before
    * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
    */
   register(/*{ strapi }*/) {},
 
   /**
    * An asynchronous bootstrap function that runs before
    * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
    */
   bootstrap({ strapi }) {
-    let { Server } = require('socket.io')
+    let { Server } = require('socket.io');
     
-    let io = new Server (strapi.server.httpServer, {
+    let io = new Server(strapi.server.httpServer, {
       cors: {
         origin: "https://quick-chat-frontend-a8cz.vercel.app",
         methods: ["POST", "GET"],
         allowedHeaders: ["my-custom-header"],
-        credentials: true
-      }
-    })
+        credentials: true,
+      },
+    });
 
-    io.on("connection", (socket)=>{
-      // console.log("A user connected", socket.id)
+    io.on("connection", (socket) => {
+      console.log("A user connected:", socket.id);
 
       socket.on("user-msg", (msgs) => {
-        // console.log("Broadcasting message:", msgs);
+        console.log("Message received:", msgs);
         socket.emit("message", msgs);
-      })
+      });
 
       socket.on("disconnect", () => {
-        // console.log("A user disconnected", socket.id)
-      })
-    })
-    
-    strapi.io = io
+        console.log("A user disconnected:", socket.id);
+      });
+    });
+
+    strapi.io = io;
   },
 };
